76c9e6834ae30888e5d42189aaa014e35a95f183
Martina <94403456+Martina2909@users.noreply.github.com>
2022-06-05 12:42:12 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+$(document).foundation()
+
+
+$('#siximg').mouseenter(function(){
+
+    $('#sixtext').show('slow');
+
+    return false;
+});
+
+
+$('#siximg').mouseleave(function(){
+
+    $('#sixtext').hide('slow');
+
+    return false;
+});
+
+
+$('#monoimg').mouseenter(function(){
+
+    $('#monotext').show('slow');
+
+    return false;
+});
+
+
+$('#monoimg').mouseleave(function(){
+
+    $('#monotext').hide('slow');
+
+    return false;
+});
